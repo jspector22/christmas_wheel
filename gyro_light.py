303677d639b3a0d31199cfc191a08647f97cdc6a
Erik Haight <erik@Eriks-MacBook-Pro.local>
2022-12-18 14:14:58 -0800

    cleanup pixel index
    - cleanup pixel index reset loop

--- gyro_light.py
+++ gyro_light.py
@@ -12,32 +12,25 @@ I2C = busio.I2C(SCL, SDA)  # uses board.SCL and board.SDA
 # Initializes L3GD20 object using default range, 250dps
 SENSOR = my_l3gd20.L3GD20_I2C(I2C, address=0x6B)
 
+# SPEED = 1  # Increase to slow down the rainbow. Decrease to speed it up.
+
 num_pixels = 95  # Update this to match the number of LEDs.
-SPEED = 1  # Increase to slow down the rainbow. Decrease to speed it up.
+sleep_interval_seconds = 0.05 # time in seconds to sleep between each pixel light up.
 pixel_pin = board.GP26  # This is the default pin on the 5x5 NeoPixel Grid BFF.
 ORDER = neopixel.GRB
 
-pixels = neopixel.NeoPixel(board.GP26, 95)
+pixels = neopixel.NeoPixel(board.GP26, num_pixels)
 
-
-i = 0
-pix_position = i % len(pixels)
-reset_led = False
+pixel_index = 0
 
 while True:
-    pixels[pix_position] = (10, 0, 0)
-    time.sleep(0.05)
-    i += 1 #only lights up pixel 0
-    pix_position += 1 #loops but goes out of range
-    if i == len(pixels):
-        reset_led = True
-        continue
-    if reset_led == True:
+    pixels[pixel_index] = (10, 0, 0)
+    time.sleep(sleep_interval_seconds)
+    pixel_index += 1 #only lights up pixel 0
+    if pixel_index > num_pixels:
         print("Reset")
         pixels.fill((0,0,0))
-        i=0
-        reset_led = False        
-        continue
+        pixel_index = 0
     #print("Angular Velocity (rad/s): {}".format(SENSOR.gyro[2]))
     #print(round(SENSOR.gyro[2], 0))
     #print()
