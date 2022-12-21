2a87e5b5cc2dc14d7001fcaabc103227a4c43afd
jspector22 <67755757+jspector22@users.noreply.github.com>
2022-12-20 21:01:14 -0700

    Update gyro_light.py
    dialed in sleep time, random light fx

--- gyro_light.py
+++ gyro_light.py
@@ -1,4 +1,5 @@
 import time
+import random
 import board
 import my_l3gd20
 import busio
@@ -15,7 +16,7 @@ SENSOR = my_l3gd20.L3GD20_I2C(I2C, address=0x6B)
 # SPEED = 1  # Increase to slow down the rainbow. Decrease to speed it up.
 
 num_pixels = 95  # Update this to match the number of LEDs.
-sleep_interval_seconds = 0.05 # time in seconds to sleep between each pixel light up.
+sleep_interval_seconds = 0.0535 # time in seconds to sleep between each pixel light up.
 pixel_pin = board.GP26  # This is the default pin on the 5x5 NeoPixel Grid BFF.
 ORDER = neopixel.GRB
 
@@ -32,29 +33,25 @@ def bound(gyro_value):
 
 def calibrate(gyro_value):
     return gyro_value + offset
-
+red = range(0,100)
+green = range(0,100)
+blue = range(0,100)
 pixel_index = 0
 
 while True:
-    if pixel_index >= num_pixels or pixel_index <= 0:
+    if abs(pixel_index) < num_pixels:
         
-        time.sleep(sleep_interval_seconds)
+        
         gyro_value = SENSOR.gyro[2]
         gyro_value = calibrate(gyro_value)
-        gyro_value = bound(gyro_value)
-        pixels.fill((0,0,0)) #clears previous pixel
-        
+        gyro_value = bound(gyro_value)      
+        pixels[pixel_index] = (random.randrange(len(red)), random.randrange(len(green)), random.randrange(len(blue)))    
+        pixel_index += round(SENSOR.gyro[2])
         print(pixel_index)
-        pixels[pixel_index] = (10, 10, 10)
-    
-        pixel_index += round(SENSOR.gyro[2]) 
+        time.sleep(sleep_interval_seconds)
         pixels.fill((0,0,0))
     if abs(pixel_index) >= num_pixels:
-
-        print("Reset")
         pixel_index = 0
+        pixels[pixel_index] = (20, 0, 0)    
+        print("Reset")        
     #print("Angular Velocity (rad/s): {}".format(SENSOR.gyro[2]))
-    #print(round(SENSOR.gyro[2], 0))
-    #print()
-    #time.sleep(1)
-    # run from 0 to 255
